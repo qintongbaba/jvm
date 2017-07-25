c7e8fe6bf5c4657bb5c3aa14ada1991b1d856c53
qintongbaba <zffwqh@126.com>
2017-07-25 21:44:00 +0800

    ch01 end

--- ch01/cmd.go
+++ ch01/cmd.go
@@ -17,11 +17,24 @@ type Cmd struct {
 //解析命令行的工具方法
 func parseCmd() *Cmd {
 	cmd := &Cmd{}
+	flag.Usage = printUsage
+	flag.BoolVar(&cmd.helpFlag, "help", false, "print help message.")
+	flag.BoolVar(&cmd.helpFlag, "?", false, "print help message.")
+	flag.BoolVar(&cmd.versionFlag, "version", false, "print version and exit.")
+	flag.BoolVar(&cmd.versionFlag, "v", false, "print version and exit.")
+	flag.StringVar(&cmd.cpOption, "classpath", "", "classpath")
+	flag.StringVar(&cmd.cpOption, "cp", "", "classpath")
+	flag.Parse() //解析失败就会调用printUsage
 
+	args := flag.Args() //获取没有被解析的参数
+	if len(args) > 0 {
+		cmd.class = args[0]
+		cmd.args = args[1:]
+	}
+	return cmd
 }
 
 //打印帮助信息
 func printUsage() {
-fmt.Printf("Usage: %s [-option] class [args]", ...)
-
+	fmt.Printf("Usage: %s [-option] class [args]\n", os.Args[0])
 }
---
+++ ch01/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	cmd := parseCmd()
+
+	if cmd.versionFlag {
+		fmt.Println("version 0.0.1")
+	} else if cmd.helpFlag {
+		printUsage()
+	} else {
+		startJVM(cmd)
+	}
+}
+
+func startJVM(cmd *Cmd) {
+	fmt.Printf("classpath:%s class:%s args:%v\n", cmd.cpOption, cmd.class, cmd.args)
+}

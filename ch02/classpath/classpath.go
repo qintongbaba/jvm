41ed43d0cca7f3c235f9f43dd6dffafbbea89416
wuqinghua <zffwqh@126.com>
2017-07-26 08:45:44 +0800

    ch02 02

--- ch02/classpath/classpath.go
+++ ch02/classpath/classpath.go
@@ -1,7 +1,25 @@
 package classpath
 
+import (
+	"os"
+)
+
 type Classpath struct {
 	bootClasspath Entry
 	extClasspath  Entry
 	userClasspath Entry
+}
+
+func Parse(jreOption, cpOption string) *Classpath                         {}
+func (self *Classpath) ReadClass(className string) ([]byte, Entry, error) {}
+func (self *Classpath) String() string                                    {}
+
+//判断目录是否存在
+func exists(path string) bool {
+	if _, err := os.Stat(path); err != nil {
+		if os.IsNotExist(err) {
+			return false
+		}
+	}
+	return true
 }

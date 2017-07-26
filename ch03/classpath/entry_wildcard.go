9c6cf272006b55915dd3a737c9f13a6f17e30d1b
wuqinghua <zffwqh@126.com>
2017-07-26 12:01:10 +0800

    ch02 end

---
+++ ch03/classpath/entry_wildcard.go
@@ -0,0 +1,33 @@
+//解析使用*的路径
+
+package classpath
+
+import (
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+func newWildcardEntry(path string) CompositeEntry {
+	baseDir := path[:len(path)-1] //remove *
+	compositeEntry := []Entry{}
+	walkFn := func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if info.IsDir() && path != baseDir {
+			return filepath.SkipDir
+		}
+
+		if strings.HasSuffix(path, ".jar") || strings.HasSuffix(path, ".Jar") {
+			jarEntry := newZipEntry(path)
+			compositeEntry = append(compositeEntry, jarEntry)
+		}
+		return nil
+	}
+
+	filepath.Walk(baseDir, walkFn)
+
+	return compositeEntry
+}
--- ch03/main.go
+++ ch03/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"jvm/ch02/classpath"
+	"jvm/ch03/classpath"
 	"strings"
 )
 
